9bdabd50dcf4706659cf7a59a11c7f0f7abffecd
mutaheer <itzmutaheer@gmail.com>
2026-02-23 13:33:15 +0530

    add experimental visulization file

---
+++ src/plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 25, 30]
+
+plt.plot(x, y)
+plt.title("Experimental Visualization")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+
